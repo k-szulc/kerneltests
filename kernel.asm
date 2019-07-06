c0bd4db5f32997148b8fbc1efa8d02ab62bba41f
k-szulc <sqn@protonmail.com>
2019-07-06 13:50:06 +0200

    Added Multiboot spec for grub booting

--- kernel.asm
+++ kernel.asm
@@ -1,6 +1,11 @@
 ;;kerne.asm
 bits 32
 section .text
+	;multiboot spec
+	align 4
+	dd 0x1BADB002 	;magic
+	dd 0x00 	;flags
+	dd - ( 0x1BADB002 - 0x00)	;checksum m+f+c should be zero
 
 global start
 extern kmain
